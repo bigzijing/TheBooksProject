b4d7d9d6302b8ab02ded81083618586350ddc5cd
Zi Jing <bigzijing@gmail.com>
2021-05-03 00:44:48 +0800

    Add more complex Book structure

--- app/models/Book.scala
+++ app/models/Book.scala
@@ -3,3 +3,21 @@ package models
 case class Book(isbn: Long, title: String, view: Int = 0, description: Option[String] = None)
 
 case class NewBook(title: String)
+
+case class RealBook(
+                   title: String,
+                   isbn: String,
+                   author: String,
+                   publisher: String,
+                   category: String,
+                   language: String,
+                   cover: String,
+                   pages: Int,
+                   condition: Option[Double] = None,
+                   rating: Option[Double] = None,
+                   grRating: Option[Double] = None,
+                   location: Option[String] = None,
+                   status: Option[String] = None,
+                   notes: Option[String] = None,
+                   read: Boolean = false
+                   )
