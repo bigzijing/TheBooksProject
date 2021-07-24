bd80ba5ec3fa80fc62207dd49c6a5cd1cf2d59bf
bigzijing <bigzijing@gmail.com>
2021-07-24 14:24:29 +0800

    Build book structure (#2)
    * Add more complex Book structure
    
    * Write a short persistent list of books
    
    * Successfully built first GET route
    
    * Add route to query by isbn
    
    * Add route to query by title
    
    * Generalize findBy methods
    
    * Added a basic function to search for books by author name
    
    * Expand tempLibrary for testing
    
    * As application/json

--- app/controllers/rest/BooksController.scala
+++ app/controllers/rest/BooksController.scala
@@ -1,7 +1,7 @@
 package controllers.rest
 
 import javax.inject._
-import models.{Book, NewBook}
+import models.{Book, NewBook, RealBook}
 import play.api._
 import play.api.libs.json._
 import play.api.mvc._
@@ -10,6 +10,54 @@ import scala.collection.mutable
 
 @Singleton
 class BooksController @Inject()(val controllerComponents: ControllerComponents) extends BaseController{
+
+  implicit val realBooksJson = Json.format[RealBook]
+  import models.TemporaryLibrary.tempLibrary
+
+  def getAllBooks(): Action[AnyContent] = Action {
+    if (tempLibrary.isEmpty) {
+      NoContent
+    } else {
+      Ok(Json.toJson(tempLibrary))
+    }
+  }
+
+  def findBookByISBN(isbn: String): Action[AnyContent] =
+    findBy(
+      tempLibrary, isbn
+    )(
+      _.isbn.replaceAll("\\s", ""),
+      isbn => isbn
+    )
+
+  def findBookByTitle(title: String): Action[AnyContent] =
+    findBy(
+      tempLibrary, title
+    )(
+      _.title.toLowerCase.replaceAll("\\s", ""),
+      _.toLowerCase
+    )
+
+  def findBooksByAuthor(author: String): Action[AnyContent] = Action {
+    val matches = tempLibrary
+      .collect {
+        case book @RealBook(_, _, bookAuthor, _, _, _, _, _, _, _, _, _, _, _, _) if {
+          bookAuthor.toLowerCase.split(" ").exists(author.contains) && author.length > 3
+        } =>
+          book
+      }
+    if (matches.nonEmpty) Ok(Json.toJson(matches)).as("application/json")
+    else NotFound
+  }
+
+  def findBy[A, B](collection: mutable.ListBuffer[RealBook], identifier: A)(f1: RealBook => B, f2: A => B): Action[AnyContent] = Action {
+    collection.find(f1(_) == f2(identifier)) match {
+      case None => NotFound
+      case Some(book) => Ok(Json.toJson(book))
+    }
+  }
+
+  //TODO: Migrate
 
   implicit val booksJson = Json.format[Book]
   implicit val newBookJson = Json.format[NewBook]
